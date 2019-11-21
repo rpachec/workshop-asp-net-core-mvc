9ee36f3feacf3d20c9ed35e937e6adb47a2267f4
Rafael Pacheco <r.reispacheco@gmail.com>
2019-11-20 23:55:29 -0300

    Sellers Controller

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        //Esse controller chama o arquivo que tiver o nome
+        //Index na pasta com o mesmo sufixo desta classe
+        //(neste caso, a pasta Sellers)
+
+        //Controlador encaminhou a requisição para a view
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
